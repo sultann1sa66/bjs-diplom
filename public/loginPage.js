2348338a1062809e5133879a6f94a9197ff1272a
Sultan Abdulazizov <sultann1.sa66@gmail.com>
2023-10-28 19:57:03 +0300

    loginpage question

---
+++ public/loginPage.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const userForm = new UserForm();
+userForm.loginFormCallback = data => {
+    ApiConnector.login(data, (response) => {
+        if(response) {
+        return result;
+        }
+        if()
+});
+}
+
+    
